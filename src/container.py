3df9de9ef9158f0f186e2a9b37bd0336e77e31ae
TingWen Liu <keri.liu@iss.nthu.edu.tw>
2025-12-10 18:42:02 +0800

    upload the container of scatter plot

---
+++ src/container.py
@@ -0,0 +1,83 @@
+from dash import Dash, dcc, html, Input, Output, State, callback
+import plotly.express as px
+
+from graph import plot_contribution_salary_scatter
+
+
+def contribution_salary_container():
+    container = html.Div([
+        # 建立 checkbox
+        dcc.RadioItems(
+            id="player-type-radio",
+            options=[
+                {"label": "batter", "value": "batter"},
+                {"label": "pitcher", "value": "pitcher"},
+            ],
+            value="batter",   # 預設選項
+            inline=True       # 兩個選項排成一列
+        ),
+        # 建立 dropdown，會根據 checkbox 的值而改變
+        dcc.Dropdown(
+            id="sub-type-dropdown",
+            value=None,
+            placeholder="please choose",
+            multi=True
+        ),
+        html.Button("Apply", id="apply-button", n_clicks=0),
+        # 取得 scatter plot，根據 checkbox 跟 dropdown 所選的值而變化
+        html.H3(
+            "Salary vs. Player Contribution",
+            style={"textAlign": "center"}
+        ),
+        dcc.Graph(id="player-scatter-graph")
+    ])
+    return container
+
+
+@callback(
+    Output("sub-type-dropdown", "options"),
+    Output("sub-type-dropdown", "value"),
+    Input("player-type-radio", "value")
+)
+def update_dropdown(player_type):
+    if player_type == "batter":
+        options = [
+            {"label": "1B", "value": "1B"},
+            {"label": "2B", "value": "2B"},
+            {"label": "3B", "value": "3B"},
+            {"label": "SS", "value": "SS"},
+            {"label": "C", "value": "C"},
+            {"label": "OF", "value": "OF"},
+            {"label": "DH", "value": "DH"},
+        ]
+        default_value = None
+    else:
+        options = [
+            {"label": "SP R", "value": "SP R"},
+            {"label": "SP L", "value": "SP L"},
+            {"label": "RP R", "value": "RP R"},
+            {"label": "RP L", "value": "RP L"}
+        ]
+        default_value = None
+
+    return options, default_value
+
+
+@callback(
+    Output("player-scatter-graph", "figure"),
+    Input("apply-button", "n_clicks"),
+    State("player-type-radio", "value"),
+    State("sub-type-dropdown", "value")
+)
+def update_scatter(n_clicks, player_type, sub_type):
+    if n_clicks == 0 or sub_type is None:
+        return px.scatter()
+    return plot_contribution_salary_scatter(
+        player_type=player_type,
+        roles=sub_type
+    )
+
+if __name__ == "__main__":
+    app = Dash()
+    app.layout = contribution_salary_container()
+    app.run(debug=True)
